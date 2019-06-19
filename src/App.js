c565f8352da2897aa771607bc0fb2ab77d1e6313
Michael Head <michaelehead@gmail.com>
2019-06-18 21:37:54 -0400

    calculations working, needs cleanup

--- src/App.js
+++ src/App.js
@@ -7,31 +7,70 @@ class App extends React.Component {
     super(props);
 
     this.calculateProbability = this.calculateProbability.bind(this);
+    this.state = {
+      letterCount: words.length,
+      letter: "",
+      matchCount: 0
+    }
   }
 
   calculateProbability(ev) {
-    const letter = ev.key;
-    const code = ev.keyCode;
+    const letter = ev.target.value.slice(-1);
+    const code = letter.charCodeAt();
+    let matchCount = 0;
 
-    if (code < 65 || code > 90) {
-      console.log("Not a letter");
+    if (ev.target.value.length === 0) {
+      return this.setState({
+        letter: "",
+        matchCount: 0
+      });
     }
 
+    if (code < 65 || code > 122) {
+      console.info("Not a letter");
+      return;
+    }
 
     const find = new RegExp(`${letter}`, "g");
     const matches = words.match(find);
 
     if (matches && matches.length) {
-      console.log("found matches", matches);
-    } else {
-      console.log("no matches for ", letter);
+      matchCount = matches.length;
     }
 
+    this.setState({
+      letter,
+      matchCount
+    });
   }
+
+  displayLetters() {
+    const letters = words.split("");
+
+    return letters.map((letter, index) => {
+      return <span key={`${letter}-${index}`} className={letter}>{letter}</span>
+    });
+  }
+
+  getPercentage() {
+    return 100 * Number.parseFloat(this.state.matchCount / this.state.letterCount).toPrecision(4);
+  }
+
   render() {
     return (
       <div>
-        <input type="text" onKeyUp={this.calculateProbability} />
+        <input type="text" onChange={this.calculateProbability} value={this.state.letter} />
+
+        {this.state.matchCount} / {this.state.letterCount} ({this.getPercentage()}%)
+
+        <div>
+
+          <style dangerouslySetInnerHTML={{
+            __html: `.${this.state.letter} { color: red }`
+          }} />
+          {this.displayLetters()}
+        </div>
+
       </div >
     );
   }
